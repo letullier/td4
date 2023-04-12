a41f764319dd29221135d2829ba4fb501af8a138
letullier <124772499+letullier@users.noreply.github.com>
2023-04-12 11:41:42 +0200

    Update code.php

--- code.php
+++ code.php
@@ -1,9 +1,23 @@
 <?php
 
-namespace UPJV\Insset\Validateur;
+namespace MonNamespace;
 
+/
+ * Nom du fichier: code.php
+ *
+ * Classe Validateur : permet de valider une chaîne de caractères
+ *
+ * Cette classe permet de valider une chaîne de caractères en vérifiant si elle est d'au moins deux caractères.
+ */
 class Validateur
 {
+    /
+     * Vérifie si une chaîne de caractères est d'au moins deux caractères de longueur.
+     *
+     * @param string $input La chaîne de caractères à vérifier.
+     *
+     * @return bool Renvoie true si la chaîne est d'au moins deux caractères de longueur, false sinon.
+     */
     public function check($input)
     {
         if (strlen($input) < 2) {
@@ -13,3 +27,5 @@ class Validateur
         }
     }
 }
+
+// End of file (EOF).
